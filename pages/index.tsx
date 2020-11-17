aeb698573c92e2338b4003ec02470443849f3e9d
Justin Aiello <justinaiello@gmail.com>
2020-11-17 15:38:50 -0500

    refactor(index): change file to typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <main>
+      <Head>
+        <title>Character Manager</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <p>Hello</p>
+    </main>
+  );
+};
+
+export default Home;
