1e0d04c8f53da0b2be5daebafd666c4a0d7b9412
Justin Aiello <justinaiello@gmail.com>
2020-11-24 20:24:46 -0500

    improvement: add background gradient

--- styles/LayoutStyles.ts
+++ styles/LayoutStyles.ts
@@ -4,4 +4,7 @@ export const StyledAppContainer = styled.main`
   display: flex;
   flex-flow: column nowrap;
   flex: 1;
+  height: 100vh;
+  background: ${({ theme }) =>
+    `linear-gradient(to bottom right, ${theme.purple} 50%, ${theme.pink})`};
 `;
