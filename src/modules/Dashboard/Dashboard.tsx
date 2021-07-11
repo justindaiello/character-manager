8ea826cd61707368040478fa6105d5202a7859c9
Justin Aiello <justinaiello@gmail.com>
2021-07-11 19:27:20 -0400

    feat: add proxy config and axios dep

--- src/modules/Dashboard/Dashboard.tsx
+++ src/modules/Dashboard/Dashboard.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import axios from 'axios'
 
 const Dashboard: React.FC = () => {
+
+  async function fetchUsers(): Promise<void> {
+    try {
+      const resp = await axios.get('/api/v1/users.json')
+
+      console.log('[resp]', resp.data)
+    } catch (err) {
+      console.log('[err]', err)
+    }
+  }
+
   return (
     <div>
-      <p>Dashboard</p>
+      <button onClick={fetchUsers}>Dashboard</button>
     </div>
   );
 };
